c66f3c51494e4cbb3e427149bbf8cb416fcacd05
Irina Pachina <tudupm@gmail.com>
2021-03-18 20:09:05 +0300

    Refactored conftest to call login/logout once for tests in test file. There will be no common session but if fixture has been created in previous test it will be used.

--- conftest.py
+++ conftest.py
@@ -3,12 +3,36 @@ import pytest
 from fixture.application import Application
 
 
+fixture = None
+
 # in pytest fixture is passed it test functions as parameter
 # this way there is no need in test class, test functions are isolated
 # to declare function as fixture use:
+
+# fixture initialization
+@pytest.fixture
+def app():
+    global fixture
+    if fixture is None:
+        fixture = Application()
+        fixture.session.login(username="admin", password="secret")
+    else:
+        if not fixture.is_valid():
+            fixture = Application()
+            fixture.session.login(username="admin", password="secret")
+    return fixture
+
+
+# fixture finalization
 # scope = "session" means that fixture is created once for all tests
-@pytest.fixture(scope = "session")
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
+# in pytest "autouse" call fixture automatically
+@pytest.fixture(scope="session", autouse=True)
+def stop(request):
+    global fixture
+
+    def finalizer():
+        fixture.session.logout()
+        fixture.destroy()
+
+    request.addfinalizer(finalizer)
     return fixture
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,12 +2,8 @@ from model.group import Group
 
 
 def test_add_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="name1", header="header1", footer="footer1"))
-    app.session.logout()
 
 
 def test_add_empty_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="", header="", footer=""))
-    app.session.logout()
