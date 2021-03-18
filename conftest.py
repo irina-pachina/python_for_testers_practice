ba8ff6608a7dc044614820b406cffbc5f1523b50
Irina Pachina <tudupm@gmail.com>
2021-03-18 20:51:31 +0300

    Refactored conftest to ensure authorization before a test, added related methods in session.

--- conftest.py
+++ conftest.py
@@ -15,11 +15,10 @@ def app():
     global fixture
     if fixture is None:
         fixture = Application()
-        fixture.session.login(username="admin", password="secret")
     else:
         if not fixture.is_valid():
             fixture = Application()
-            fixture.session.login(username="admin", password="secret")
+    fixture.session.ensure_login(username="admin", password="secret")
     return fixture
 
 
@@ -31,7 +30,7 @@ def stop(request):
     global fixture
 
     def finalizer():
-        fixture.session.logout()
+        fixture.session.ensure_logout()
         fixture.destroy()
 
     request.addfinalizer(finalizer)
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,7 +3,8 @@ from model.group import Group
 
 def test_add_group(app):
     app.group.create(Group(name="name1", header="header1", footer="footer1"))
-
+    # imitate occasional logout or failed test to check function "ensure_login"
+    # app.session.logout()
 
 def test_add_empty_group(app):
     app.group.create(Group(name="", header="", footer=""))
