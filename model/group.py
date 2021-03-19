66d85092eb8f7ae6bc8cfd35cee071e5e0e972a7
Irina Pachina <tudupm@gmail.com>
2021-03-19 21:07:19 +0300

    Added assertions that compares group's data in tests modify_group and add_group.

--- model/group.py
+++ model/group.py
@@ -1,3 +1,6 @@
+from sys import maxsize
+
+
 class Group:
     def __init__(self, name=None, header=None, footer=None, id=None):
         self.name = name
@@ -9,4 +12,14 @@ class Group:
         return f"name = {self.name}, id = {self.id}"
 
     def __eq__(self, other):
-        return self.name == other.name and self.id == other.id
+        # test data doesn't have id and such elements in old_groups should be compared only by name
+        return self.name == other.name and (self.id == other.id or self.id is None or other.id is None)
+
+    # when group list is sorted with added test data (old_groups) id is None.
+    # method id_or_max makes element with empty id the last one
+    def id_or_max(self):
+        if self.id:
+            return int(self.id)
+        else:
+            # mazsize is max id in lists, usually used in python as maximum integer constant
+            return maxsize
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -5,15 +5,25 @@ def test_modify_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="to_modify", header="header_mod", footer="footer_mod"))
     old_groups = app.group.get_group_list()
-    app.group.modify_first_group(Group(name="new name"))
+    group = Group(name="new name")
+    group.id = old_groups[0].id
+    app.group.modify_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 def test_modify_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(name="to_modify", header="header_mod", footer="footer_mod"))
     old_groups = app.group.get_group_list()
-    app.group.modify_first_group(Group(header="new header"))
+    group = Group(header="header")
+    group.name = old_groups[0].name
+    group.id = old_groups[0].id
+    app.group.modify_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
