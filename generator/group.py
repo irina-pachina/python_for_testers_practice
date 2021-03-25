13ed36c0e6be6c0632e54fd3aab42a6d5e5f134e
Irina Pachina <tudupm@gmail.com>
2021-03-25 08:37:35 +0300

    Package 'generator' is added to generate testdata into file. generator 'group' is created. Now json file can be used instead of data/add_group.py.

--- generator/group.py
+++ generator/group.py
@@ -3,6 +3,25 @@ import random
 import string
 import os.path
 import json
+import getopt
+import sys
+
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of groups", "file"])
+except getopt.GetoptError as err:
+    getopt.usage()
+    sys.exit(2)
+
+# default parameters
+n = 2
+f = "data/groups.json"
+# in parameters of script specify "-n 10 -f data/test.json"
+for o, a in opts:
+    if o == "-n":
+        n = int(a)
+    elif o == "-f":
+        f = a
 
 
 # if generated ' symbol (string.punctuation) will cause failed test (it is a bug in the app)
@@ -14,7 +33,7 @@ def random_string(prefix, maxlen):
 # 1 case with all empty fields and 2 cases with all filled fields
 testdata = [Group(name="", header="", footer="")] + \
            [Group(name=random_string("name", 5), header=random_string("header", 5), footer=random_string("footer", 5))
-            for i in range(2)]
+            for i in range(n)]
 
 # 8 cases with combinations empty/filled
 # testdata = [Group(name=name, header=header, footer=footer)
@@ -23,7 +42,7 @@ testdata = [Group(name="", header="", footer="")] + \
 #             for footer in ["", random_string("footer", 5)]]
 
 
-file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../data/groups.json")
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
-with open(file, "w") as f:
-    f.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=4))
+with open(file, "w") as out:
+    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=4))
