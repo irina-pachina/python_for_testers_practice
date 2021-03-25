fb6608b36e2944c5b8035221b44965e004018ba6
Irina Pachina <tudupm@gmail.com>
2021-03-25 11:59:14 +0300

    Fixture for db is added.

--- conftest.py
+++ conftest.py
@@ -5,32 +5,52 @@ import os.path
 import importlib
 import jsonpickle
 from fixture.application import Application
+from fixture.database import DataBase
 
 
 fixture = None
 target = None
 
 
-# in pytest fixture is passed it test functions as parameter
-# this way there is no need in test class, test functions are isolated
-# to declare function as fixture use:
-# fixture initialization
-@pytest.fixture
-def app(request):
-    global fixture
+def load_config(config):
     global target
-    browser = request.config.getoption("--browser")
     if target is None:
         # w/o __file__ 'tricks' "target.json" will tried to be open in current directory
         # i.e. in ../test or Configuration>Working Directory
         # thus config path should be stored in root directory and 'built' from current one
-        config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), config)
         with open(config_path) as file:
             target = json.load(file)
+    return target
+
+
+# in pytest fixture is passed it test functions as parameter
+# this way there is no need in test class, test functions are isolated
+# to declare function as fixture use:
+# fixture initialization
+# this fixture use class Application to work only with browser
+@pytest.fixture
+def app(request):
+    global fixture
+    browser = request.config.getoption("--browser")
+    web_config = load_config(request.config.getoption("--target"))["web"]
     if fixture is None or not fixture.is_valid():
-        fixture = Application(browser=browser, stand_url=target["standURL"])
-    fixture.session.ensure_login(username=target["username"], password=target["password"])
+        fixture = Application(browser=browser, stand_url=web_config["standURL"])
+    fixture.session.ensure_login(username=web_config["username"], password=web_config["password"])
     return fixture
+
+
+# this fixture use class DataBase to work only with db
+@pytest.fixture(scope="session")
+def db(request):
+    db_config = load_config(request.config.getoption("--target"))["db"]
+    db_fixture = DataBase(host=db_config["host"], name=db_config["name"], user=db_config["user"], password=db_config["password"])
+
+    def finalizer():
+        db_fixture.destroy()
+
+    request.addfinalizer(finalizer)
+    return db_fixture
 
 
 # fixture finalization
---
+++ fixture/database.py
@@ -0,0 +1,13 @@
+import mysql.connector
+
+
+class DataBase:
+    def __init__(self, host, name, user, password):
+        self.host = host
+        self.name = name
+        self.user = user
+        self.password = password
+        self.connection = mysql.connector.connect(host=host, database=name, user=user, password=password)
+
+    def destroy(self):
+        self.connection.close()
