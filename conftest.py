e0e7bf21edc3602b46ae6005c5d59cd15f3311c3
Irina Pachina <tudupm@gmail.com>
2021-03-25 09:44:06 +0300

    Another type of fixture is added in pytest_generate_tests: notation "json_filename" is used, i.e. in such tests test data is loaded from 'data/filename.json'. jsonpickle is used to auto specify which class is encoded in json, i.e. "py/object": "model.group.Group" in case of contact groups.

--- conftest.py
+++ conftest.py
@@ -3,6 +3,7 @@ import pytest
 import json
 import os.path
 import importlib
+import jsonpickle
 from fixture.application import Application
 
 
@@ -54,6 +55,7 @@ def pytest_addoption(parser):
     # the rest of parameters are stored in config file
     parser.addoption("--target", action="store", default="target.json")
 
+
 # pytest_generate_tests hook function is called when collecting a test function
 # to make parametrization dynamic and custom
 def pytest_generate_tests(metafunc):
@@ -61,7 +63,15 @@ def pytest_generate_tests(metafunc):
         if fixture.startswith("data_"):
             testdata = load_from_module(fixture[5:])
             metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+        elif fixture.startswith("json_"):
+            testdata = load_from_json(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
 
 
 def load_from_module(module):
     return importlib.import_module("data.%s" % module).testdata
+
+
+def load_from_json(file):
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
+        return jsonpickle.decode(f.read())
--- generator/group.py
+++ generator/group.py
@@ -2,7 +2,7 @@ from model.group import Group
 import random
 import string
 import os.path
-import json
+import jsonpickle
 import getopt
 import sys
 
@@ -45,4 +45,7 @@ testdata = [Group(name="", header="", footer="")] + \
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
 with open(file, "w") as out:
-    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=4))
+    # out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=4))
+    # instead of 'json' using 'jsonpickle'
+    jsonpickle.set_encoder_options("json", indent=4)
+    out.write(jsonpickle.encode(testdata))
