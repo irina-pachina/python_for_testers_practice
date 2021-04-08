8489ccdf2d17fa0f9609bc1bb470283f26bf7f92
Irina Pachina <tudupm@gmail.com>
2021-04-08 09:10:30 +0300

    @allure.step decorator is added to test add_group.

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,14 +1,18 @@
 from model.group import Group
+import allure
 
 
 def test_add_group(app, db, json_groups):
     group = json_groups
-    old_groups = db.get_group_list()
-    app.group.create(group)
+    with allure.step("Given a group list"):
+        old_groups = db.get_group_list()
+    with allure.step("When I add the group %s to the list" % group):
+        app.group.create(group)
     # imitate occasional logout or failed test to check function "ensure_login"
     # app.session.logout()
     # hashing app.group.count() is redundant due to long execution in contrast with db request
     # assert len(old_groups) + 1 == app.group.count()
-    new_groups = db.get_group_list()
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    with allure.step("Then the new group list is equal to the old list with the added group"):
+        new_groups = db.get_group_list()
+        old_groups.append(group)
+        assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
